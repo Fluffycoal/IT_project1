9c8e4e1864048de437999a681a098d6b1d865461
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2025-07-04 01:34:17 +0300

    fixing errors

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-function scrollCarousel(direction) {
-  const container = document.getElementById('popularCarousel');
-  const scrollAmount = 150; // adjust as needed
-  container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
-}
